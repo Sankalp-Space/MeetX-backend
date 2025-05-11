e33cce705ff9fabeeb1cd0eabe07cc5e6f5c1ef6
Sankalp Shrivastav <133688351+Sankalp-Space@users.noreply.github.com>
2025-05-11 23:37:38 +0530

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,14 +3,14 @@ import User from '../models/User.js';
 
 const protect = async (req, res, next) => {
     let token = req.headers.authorization?.split(' ')[1];
-    if (!token) return res.status(401).json({ message: 'Not authorized to access LOL' });
+    if (!token) return res.status(401).json({ message: ' Unauthorized to access' });
 
     try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = await User.findById(decoded.id).select('-password');
     next();
     } catch (error) {
-    res.status(401).json({ message: 'Token Galat he ' });
+    res.status(401).json({ message: 'Invalid Token ' });
     }
 };
 
